edb05441e0c25d3811fb537f398c76af92b1938c
jianyiee96 <hew1521@hotmail.com>
2015-07-20 16:11:08 +0800

    Initial Commit.

---
+++ src/assignment2final/Assignment2Final.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2final;
+
+/**
+ *
+ * @author JianYiee
+ */
+public class Assignment2Final {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String url = "";
+        System.out.println(PageRead.readPage(url));
+    }
+    
+}
