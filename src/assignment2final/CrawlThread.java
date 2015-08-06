60a64563c708090df4e66f59267b6f3cfbbe8b2f
jianyiee96 <hew1521@hotmail.com>
2015-08-06 21:17:49 +0800

    Completed. Left to upgrade and debug program

---
+++ src/assignment2final/CrawlThread.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2final;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.sound.midi.Patch;
+
+/**
+ *
+ * @author JianYiee
+ */
+public class CrawlThread extends Thread {
+
+    private String buffer;
+    private int pageRequired;
+    private ArrayList<String> liveThread;
+    private LinkedList linkedUrl;
+    private HashMap<String, Integer> antiDuplicate;
+    private HashMap<String, String> urlProcessed = new HashMap<>();
+
+    public CrawlThread(String buffer, HashMap<String, String> urlProcessed, int pageRequired, LinkedList linkedUrl, HashMap<String, Integer> antiDuplicate, ArrayList<String> liveThread) {
+        this.buffer = buffer;
+        this.liveThread = liveThread;
+        this.urlProcessed = urlProcessed;
+        this.pageRequired = pageRequired;
+        this.linkedUrl = linkedUrl;
+        this.antiDuplicate = antiDuplicate;
+
+    }
+
+    @Override
+    public void run() {
+        liveThread.add(buffer);
+        buffer = buffer.replace(" ", "%20");
+        System.out.println(buffer);
+
+        StringBuilder source = PageRead.readPage(buffer);
+
+        if (source.toString().equals("")) {
+            buffer = buffer.replace("http", "https");
+            source = PageRead.readPage(buffer);
+
+            if (source.toString().equals("")) {
+                buffer = buffer.replace("https://", "");
+
+            source = PageRead.readPage(buffer);
+
+            }
+
+        }
+        System.out.println("putting "+buffer);
+        urlProcessed.put(buffer, source.toString());
+        System.out.println("putted"+buffer);
+        if (urlProcessed.size() < pageRequired) {
+            System.out.println("DONE WITH " + urlProcessed.size());
+           // String regex ="^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+            // Pattern pattern = Pattern.compile(regex);
+            Pattern pattern = Pattern.compile("href=\\\"https?:\\/\\/[a-zA-Z-_]*\\.[a-zA-Z0-9-_]*\\.[a-zA-Z0-9-_]*(/[a-zA-Z0-9\\.\\?=&-_]*)*");
+            Matcher matcher = pattern.matcher(source);
+
+            while (matcher.find()) {
+                String link = matcher.group().substring(6);
+                if (!antiDuplicate.containsKey(link.toLowerCase()) && !link.contains(".css")) {
+                    antiDuplicate.put(link.toLowerCase(), antiDuplicate.size());
+                    linkedUrl.add(link);
+
+                }
+
+            }
+
+        }
+
+        liveThread.remove(0);
+
+    }
+}
---
+++ src/assignment2final/SearchEngine.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2final;
+
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.LinkedList;
+import javafx.animation.Animation;
+/**
+ *
+ * @author JianYiee
+ */
+public class SearchEngine extends Thread {
+
+    private String query;
+    private LinkedList linkedUrl;
+    private HashMap<String,Integer> antiDuplicate;
+    final private String engine;
+   
+    public SearchEngine(String query, String engine, LinkedList linkedUrl,HashMap<String,Integer> antiDuplicate) {
+        this.query = query;
+        this.engine = engine;
+        this.linkedUrl = linkedUrl;
+        this.antiDuplicate = antiDuplicate;
+    }
+
+    @Override
+    public void run() {
+           query = query.replace(" ", "%20");
+           System.out.println("Searching "+engine+query);
+           StringBuilder source = PageRead.readPage(engine+query);
+           
+           
+           Pattern pattern = Pattern.compile("<cite>.*?<\\/cite>");
+           Matcher matcher = pattern.matcher(source);
+           
+           Pattern pattern2 = Pattern.compile("web-result-url.*?<\\/p>");
+           Matcher matcher2 = pattern2.matcher(source);
+           
+           if (matcher.find()){
+               String link = matcher.group();
+               link = link.replace("<cite>", "");
+               link = link.replace("<strong>","");
+               link = link.replace("</strong>","");
+               link = link.replace("</cite>","");
+               System.out.println("Link found: "+link);
+               
+               if (!antiDuplicate.containsKey(link.toLowerCase())){
+                    antiDuplicate.put(link.toLowerCase(), antiDuplicate.size());
+                    linkedUrl.add("http://"+link);
+               } else {
+                   System.out.println("Duplicate found, skipping...");
+               }
+           } else if(matcher2.find()) {
+               String link = matcher2.group();
+               link = link.replace("web-result-url\">", "");
+               link = link.replace("</p>", "");
+               System.out.println("Link found: "+link);
+               
+               if (!antiDuplicate.containsKey(link.toLowerCase())){
+                    antiDuplicate.put(link.toLowerCase(), antiDuplicate.size());
+                    linkedUrl.add("http://"+link);
+               } else {
+                   System.out.println("Duplicate found, skipping...");
+               }
+               
+           } else {
+               System.out.println("No matches for "+engine+query);
+           }
+                 
+           
+    }
+
+}
